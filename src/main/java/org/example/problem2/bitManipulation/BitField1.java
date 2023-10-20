097fffde390b0c4ffc8688c92d54a7736d08f3c8
Dinuja Pinto <sri.prabasha@gmail.com>
2023-10-20 13:20:45 +0530

    bitField

--- src/main/java/org/example/problem2/bitManipulation/BitField1.java
+++ src/main/java/org/example/problem2/bitManipulation/BitField1.java
@@ -5,24 +5,27 @@ import java.time.LocalTime;
 
 public class BitField1 {
 
-    public static int LOG_TIME = 1;   // 0001
-    public static int LOG_DATE = 2;   // 0010
-    public static int LOG_COUNT = 4;   // 0100
+    public static int LOG_TIME = 0x01;   // 0001
+    public static int LOG_DATE = 0x02;   // 0010
+    public static int LOG_COUNT = 0x04;   // 0100
 
     public static void logMsg(String msg, int options){
         int logCount=0;
         LocalDate localDate = LocalDate.now();
         LocalTime localTime = LocalTime.now();
 
-        if( (options & LOG_COUNT) != 0 )
+        if( checkOption(options, LOG_COUNT) )
             System.out.println(++logCount);
-        if( (options & LOG_DATE) != 0 )
+        if( checkOption(options, LOG_DATE) )
             System.out.println(localDate);
-        if( (options & LOG_TIME) != 0 )
+        if( checkOption(options, LOG_TIME) )
             System.out.println(localTime);
         System.out.println(msg);
     }
 
+    public static boolean checkOption(int options, int flg){
+        return (options & flg) != 0;
+    }
     public static void main(String[] args) {
         logMsg("Hello", LOG_DATE | LOG_TIME | LOG_COUNT);
         System.out.println();
